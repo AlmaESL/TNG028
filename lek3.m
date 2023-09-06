af68a0dcc91d5b17ad4bf1326427b05023a994f2
AlmaESL <91621109+AlmaESL@users.noreply.github.com>
2023-09-06 11:29:49 +0200

    Add files via upload

--- lek3.m
+++ lek3.m
@@ -55,36 +55,124 @@ grid on;
 
 s=tf('s'); 
 Ga = 1/(s^2+2*s+1)
-
+figure(2); 
 step(Ga)
 grid on
 
-
 %visualize poles 
 figure(3);
-plot((pole(Ga)), 'x'); 
+plot(real(pole(Ga)),imag(pole(Ga)), "xg");
+title('poles') ;
+xlabel('Re');
+ylabel('Im'); 
 
 grid on
 
-Gb = 1/(s^2+0.4*s+1); 
+%%
+%2.6 b) 
+s=tf('s'); 
+
+Gb = 1/(s^2+0.4*s+1) 
 figure(2); 
 step(Gb); 
+grid on
 pole(Gb)
 figure(3); 
-plot((real(pole(Gb)), imag(pole(Ga)), 'o'); 
+plot(real(pole(Gb)),imag(pole(Gb)), "xr");
 
+title('poles') ;
+xlabel('Re');
+ylabel('Im'); 
+
+grid on 
+%%
+%2.6 c) 
+s=tf('s');
 
 Gc = 1/(s^2+5*s+1)
 figure(2); 
 step(Gc); 
+grid on
 pole(Gc); 
 figure(3);
-plot(pole(Gc), 'x');
+plot(real(pole(Gc)),imag(pole(Gc)), "xb"); 
+title('poles') ;
+xlabel('Re');
+ylabel('Im'); 
+grid on 
 
+%%
+%2.6 d)
+s=tf('s'); 
 
 Gd = 1/(s^2+s+1)
 figure(2); 
 step(Gd); 
+grid on
 pole(Gd)
 figure(3); 
-plot(pole(Gd),  'o'); 
+plot(real(pole(Gd)),imag(pole(Gd)), "xm");
+title('poles') ;
+xlabel('Re');
+ylabel('Im'); 
+grid on 
+%%
+%2.6 e) 
+s=tf('s'); 
+
+Ge = 4/(s^2+2*s+4)
+figure(2); 
+step(Ge);
+grid on
+pole(Ge)
+figure(3); 
+plot(real(pole(Ga)),imag(pole(Ga)), "xb"); 
+title('poles');
+xlabel('Re');
+ylabel('Im'); 
+grid on 
+%%
+%2.7
+
+s=tf('s') ;
+alpha = -0.2; 
+G_s = (alpha*s+1)/(s^2+2*s+1); 
+step(G_s); 
+grid on; 
+
+%%
+%2.4 a) 
+s=tf('s'); 
+
+G_ref = 1/(s^2+s+1); 
+figure(1); 
+step(G_ref); 
+title('reference step response'); 
+grid on; 
+
+G_A_pos = 1/(s^2+2*s+1); 
+figure(2); 
+step(G_A_pos); 
+title('step response a > 1');
+grid on; 
+
+G_A_neg = 1/(s^2+0.5*s+1);
+figure(3); 
+step(G_A_neg); 
+title('step response a < 1'); 
+grid on; 
+
+%we see that a < 1 gives the oscillations we need to match the solid line
+%of the given plot that illustrates Ga
+
+%%
+%2.4 b) 
+s=tf('s'); 
+b=2; 
+G_B = (b^2)/(s^2+b*s+(b^2))
+step(G_B);
+title('step response of Gb'); 
+grid on
+
+%genom att testa oss fram med olika värden på b så finner vi att b=2 ger
+%det stegsvaret som visas i uppgiftens plot 
